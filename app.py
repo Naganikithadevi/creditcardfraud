76b5d13ebbd247e6c57f78d067442aa596768bd5
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2023-12-29 16:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 import scikit_learn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import streamlit as st
+
 from PIL import Image
 
 # create Streamlit app
