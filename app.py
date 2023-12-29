a7ef9abf9ad2c577fedc5a2d52258f6e1aae6cf5
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2023-12-29 16:08:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 from PIL import Image
 
