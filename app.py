07b312937e2d67f22e6716cc775b088ae2b56e27
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2023-12-29 16:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import streamlit as st
+from PIL import Image
 
 # create Streamlit app
 st.title("Credit Card Fraud Detection Model")
