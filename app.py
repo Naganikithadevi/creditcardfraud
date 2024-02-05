8979c4e21a58b84a49ce9eb7762ef681156f2074
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2024-02-05 15:31:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ if submit:
     features = np.array(input_df_lst, dtype=np.float64)
     # make prediction
     model = load('creditcardmodel.joblib')
+    st.write("model loaded")
     prediction = model.predict(features.reshape(1,-1))
     # display result
     if prediction[0] == 0:
