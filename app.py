bab75cb82fb82d905ed5c66141b7e8af15561cc6
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2024-02-05 20:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,26 +9,7 @@ from PIL import Image
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-dump(model, "creditcardmodel.joblib")
 
-st.title("Credit Card Fraud Detection Model")
-st.write("Enter the following features to check if the transaction is legitimate or fraudulent:")
-input_df = st.text_input('Input All features')
-
-submit = st.button("Submit")
-
-# Load the image
-image = Image.open('your_image_url.jpg')
-st.image(image)
-
-import numpy as np
-import pandas as pd
-import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from joblib import dump, load
-from PIL import Image
 
 # Assuming X_train and y_train are defined before this point
 # Make sure to replace 'your_image_url.jpg' with the actual path or URL of your image
