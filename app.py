4f44b7da1f9261079474bfd9cbb1ce8b4cf6e751
Naganikithadevi <111067674+Naganikithadevi@users.noreply.github.com>
2024-02-05 20:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,14 +24,10 @@ st.image(image)
 if submit:
     # Split the input string into a list of features
     input_df_lst = [float(x.strip()) for x in input_df.split(',')]
-        # Reshape and convert to numpy array
     features = np.array(input_df_lst).reshape(1, -1)
-
-        # Make prediction
     prediction = model.predict(features)
-
-        if prediction[0] == 0:
+    if prediction[0] == 0:
             st.write("Legitimate transaction")
-        else:
+    else:
             st.write("Fraudulent transaction")
      
